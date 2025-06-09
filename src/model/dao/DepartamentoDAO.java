603e83648ac2418b0f49d5c1e57ecc92f87a439a
Your Name <you@example.com>
2025-06-09 19:19:41 -0300

    DepartamentoDAO e VendedorDAO interfaces

---
+++ src/model/dao/DepartamentoDAO.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entidades.Departamento;
+
+public interface DepartamentoDAO {
+
+	void insert(Departamento obj);
+	void update(Departamento obj);
+	void deleteById(Integer id);
+	void findById(Integer id);
+	List<Departamento> findAll();
+	
+	
+}
